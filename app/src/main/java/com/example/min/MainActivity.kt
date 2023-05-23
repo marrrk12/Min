a9b3e8ef430bc7aec52c578bd9a8bb4618ef5476
Marrrk <mark1209@yandex.ru>
2023-05-23 20:52:07 +0500

    try catch

--- app/src/main/java/com/example/min/MainActivity.kt
+++ app/src/main/java/com/example/min/MainActivity.kt
@@ -3,8 +3,12 @@ package com.example.min
 import android.content.pm.PackageManager
 import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
+import android.graphics.Typeface
 import android.os.Build
 import android.os.Bundle
+import android.view.View
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
@@ -15,6 +19,7 @@ import com.google.android.gms.location.LocationServices
 import com.google.android.gms.location.Priority
 import com.squareup.picasso.Picasso
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import java.io.IOException
 import java.sql.SQLException
@@ -27,10 +32,14 @@ class MainActivity : AppCompatActivity() {
     private lateinit var mDb: SQLiteDatabase
     private lateinit var fusedLocationClient: FusedLocationProviderClient
     private lateinit var  countyofuser: TextView
+    private lateinit var  fact1: TextView
+    private lateinit var  fact2: TextView
+    private lateinit var  fact3: TextView
     private lateinit var  street: TextView
     private lateinit var  listView: ListView
     private lateinit var img_of_county : ImageView
-    private var arr_of_place : Array<String> =  arrayOf("undefined", "undefined", "undefined")
+    private lateinit var arradapter: ArrayAdapter<String>
+    private var arr_of_place : Array<String> =  arrayOf("undefined")
 
     private var address: Address? = Address("undefined",
         "undefined","undefined","undefined","undefined","undefined")
@@ -40,15 +49,31 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        listView = findViewById(R.id.listView)
+//        listView = findViewById(R.id.listView)
+//        arradapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, arr_of_place)
+//        listView.adapter = arradapter
+//        adapter.remove("undefined")
+//        listView.adapter.notifyDataSetChanged()
 
         countyofuser = findViewById(R.id.textView)
-        //street = findViewById(R.id.textView2)
+        fact1 = findViewById(R.id.fact1)
+        fact2 = findViewById(R.id.fact2)
+        fact3 = findViewById(R.id.fact3)
         img_of_county = findViewById(R.id.img_of_county)
         requestPermission()
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
-
         mDBHelper = DatabaseHelper(this)
+
+        val animScale: Animation = AnimationUtils.loadAnimation(this, R.anim.scale)
+        val btnScale = findViewById<View>(R.id.button_r) as ImageButton
+        btnScale.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(view: View) {
+                view.startAnimation(animScale)
+                getLoc()
+            }
+        })
+
+
 
         try {
             mDBHelper.updateDataBase()
@@ -63,19 +88,14 @@ class MainActivity : AppCompatActivity() {
         }
 
         //getLoc()
-
-
-
         //setPicture()
 
 
         lifecycleScope.launch(Dispatchers.Main) {
             getLoc()
+            setPicture()
         }
-        listView.adapter =
-            ArrayAdapter(this, android.R.layout.simple_list_item_1, arr_of_place)
 
-        val i = 0
 
     }
 
@@ -89,16 +109,27 @@ class MainActivity : AppCompatActivity() {
             lifecycleScope.launch(Dispatchers.IO)
             {
                 address = getPlace(it.latitude, it.longitude)
-                launch(Dispatchers.Main)
-                {
-                    countyofuser.text ="Вы прибываете в: " + address?.county ?: "undefined"
-                    if (countyofuser.text != "undefined")
-                        setPicture()
+                if (address!=null) {
+                    launch(Dispatchers.Main)
+                    {
+                        countyofuser.text = "Вы прибываете в: " + address?.county
+                        if (countyofuser.text != "undefined")
+                            setPicture()
+                    }
+                } else{
+                    launch(Dispatchers.Main)
+                    {
+                        countyofuser.text = "Что-то пошло не так "
+
+                    }
                 }
             }
         }
     }
 
+    fun OnclickButton(view: View) {
+        getLoc()
+    }
 
     private fun setPicture() {
         var mDrawableName = ""
@@ -111,25 +142,24 @@ class MainActivity : AppCompatActivity() {
             val cursor: Cursor = mDb.rawQuery("SELECT * FROM table_of_rngr WHERE name = '$county'", null)
             cursor.moveToFirst()
 
-//            while (!cursor.isAfterLast) {
-                mNamecounty = cursor.getString(0)
-                mDrawableName = cursor.getString(1)
+            mNamecounty = cursor.getString(0)
+            mDrawableName = cursor.getString(1)
 
-//                cursor.moveToNext()
-//            }
             cursor.close()
 
-            val i = 0
-
-
-            val cursor2: Cursor = mDb.rawQuery("SELECT * FROM table_of_places WHERE id = (SELECT id_pl FROM table_of_rngr WHERE name = '" + county + "')", null)
+            val cursor2: Cursor = mDb.rawQuery("SELECT * FROM table_of_places WHERE id = (SELECT id_pl FROM table_of_rngr WHERE name = '$county')", null)
             cursor2.moveToFirst()
 
-            while (!cursor2.isAfterLast) {
-                arr_of_place = arrayOf(cursor2.getString(1),cursor2.getString(2),cursor2.getString(3))
-                cursor2.moveToNext()
+            if (countyofuser.text != "undefined")
+            {
+                fact1.text = cursor2.getString(1)
+                fact2.text = cursor2.getString(2)
+                fact3.text = cursor2.getString(3)
+
             }
             cursor2.close()
+
+
 
             val resID = resources.getIdentifier(mDrawableName, "drawable", packageName)
             lifecycleScope.launch(Dispatchers.Main)
@@ -144,10 +174,19 @@ class MainActivity : AppCompatActivity() {
 
 
     private suspend fun getPlace(late: Double, long: Double): Address? {
-        val addressApi = RetrofitHelperSight.getInstance().create(AddressApi::class.java)
-        val result = addressApi.getAddress(late, long)
-        if (result != null)
-            return result.body()?.address
+        try {
+            val addressApi = RetrofitHelperSight.getInstance().create(AddressApi::class.java)
+            val result = addressApi.getAddress(late, long)
+            if (result != null) {
+                return result.body()?.address
+
+            }
+        }
+        catch (e: Exception) {
+            println(e.message)
+        }
+
+
         return null
     }
 
@@ -160,6 +199,7 @@ class MainActivity : AppCompatActivity() {
             ), 1)
         }
     }
+
     // Function to check and request permission.
     private fun checkPermission(permission: String, requestCode: Int) {
         if (ContextCompat.checkSelfPermission(this@MainActivity, permission) == PackageManager.PERMISSION_DENIED) {
@@ -169,5 +209,7 @@ class MainActivity : AppCompatActivity() {
             Toast.makeText(this@MainActivity, "Permission already granted", Toast.LENGTH_SHORT).show()
         }
     }
+
+
 
 }
